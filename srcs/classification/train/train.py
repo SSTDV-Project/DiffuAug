de32bea138ef085b549f59f05cde61dfa877a574
js <somadubel@gmail.com>
2024-06-15 08:22:08 +0000

    [Feat: classification] AUC 계산. 그러나 제대로 동작하지 않는 코드와 구분이 필요하다.

--- srcs/classification/train/train.py
+++ srcs/classification/train/train.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 from torchvision.models.resnet import ResNet18_Weights
+from torchvision.models import resnet18
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 
@@ -21,10 +22,10 @@ def load_model(device, mode=None):
     # Renset
     if mode=="resnet18":
         model = models.resnet18(pretrained=False)
-        model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7))
+        model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         num_ftrs = model.fc.in_features # 마지막 계층의 입력 특징의 수를 가져옴
-        model.fc = nn.Linear(num_ftrs, 1)  # 마지막 계층을 새로운 클래스 수에 맞게 교체 (여기서는 10개 클래스)
-
+        model.fc = nn.Linear(num_ftrs, 1) 
+        
         model = model.to(device=device)    
     # Alexnet
     elif mode=="alexnet":
@@ -47,7 +48,7 @@ def train_one_epoch(cfg, model, optimizer, loss_func, train_loader, batch_size, 
     모델을 한 epoch 동안 훈련합니다.
 
     Args:
-        net: 학습시킬 모델
+        model: 학습시킬 모델
         optimizer: 사용할 optimizer
         loss_func: 사용할 loss 함수
         trainloader: 학습 데이터를 담고 있는 DataLoader
@@ -64,13 +65,10 @@ def train_one_epoch(cfg, model, optimizer, loss_func, train_loader, batch_size, 
     
     y_scores_list = list()
     y_true_list = list()
-    epoch_loss = 0.0
-    
-    # Loss 클래스 이름 가져오기
-    loss_name = loss_func.__class__.__name__
+    epoch_loss = 0.0    
 
     # 훈련 데이터에 대해 DataLoader를 반복합니다.
-    for idx, data in enumerate(train_loader, 0):
+    for idx, data in enumerate(train_loader):
         # 입력을 가져오기
         inputs, targets, _ = data
         inputs = inputs.to(device=device, dtype=torch.float32)
@@ -80,10 +78,7 @@ def train_one_epoch(cfg, model, optimizer, loss_func, train_loader, batch_size, 
         logit = model(inputs)        
         prob = torch.sigmoid(logit)
         
-        if loss_name == "BCELoss":
-            loss = loss_func(prob, targets)
-        elif loss_name == "BinaryFocalLoss":
-            loss = loss_func(logit, targets)
+        loss = loss_func(prob, targets)
 
         # 값이 0인 텐서를 만든 후, 임계값을 기준으로 값을 1로 설정
         pivot = prob > cfg.params.threshold
@@ -112,7 +107,7 @@ def train_one_epoch(cfg, model, optimizer, loss_func, train_loader, batch_size, 
     # 모든 배치에 대한 예측 확률과 실제 레이블을 하나의 배열로 합치기
     y_scores = np.array(y_scores_list)
     y_true = np.array(y_true_list)
-    
+
     # AUC 및 Epoch 평균 loss 계산
     auc_scroe = roc_auc_score(y_true, y_scores)
     epoch_loss = round(epoch_loss / len(train_loader), 3)
@@ -123,16 +118,21 @@ def train_one_epoch(cfg, model, optimizer, loss_func, train_loader, batch_size, 
     return model, epoch_loss
 
 
-def valid_one_epoch(model, val_loader, epoch, device):
+def valid_one_epoch(model, val_loader, cur_epoch, device):
     model.eval()
-    correct, total = 0, 0
+
+    total = 0
+    correct = 0
     
     # ROC 계산을 위한 리스트 초기화
     y_scores_list = list()
     y_true_list = list()
     
+    print("y_scores_list: ", y_scores_list)
+    print("y_true_list: ", y_true_list)
+    
     # 테스트 데이터를 반복하며 예측값을 생성한다
-    for batch_idx, data in enumerate(val_loader, 0):
+    for batch_idx, data in enumerate(val_loader):
         # 입력을 가져오기
         inputs, targets, _ = data
         inputs = inputs.to(device=device, dtype=torch.float32)
@@ -149,18 +149,23 @@ def valid_one_epoch(model, val_loader, epoch, device):
         predicted[threshold] = 1.0
         
         # 정확도 계산
-        total += targets.size(0)
+        total += predicted.size(0)
         correct += (predicted == targets).sum().item()
         
         # ROC 계산을 위해 값 저장
         y_scores_list.extend(prob.detach().cpu().numpy())
-        y_true_list.extend(targets.cpu().numpy())        
+        y_true_list.extend(targets.cpu().numpy())
+
+    y_scores = np.array(y_scores_list)
+    y_true = np.array(y_true_list)
+    print("y_scores.shape: ", y_scores.shape)
+    print("y_true.shape: ", y_true.shape)
 
     # accuracy 출력 
     acc = 100.0 * (correct / total)
-    auc = roc_auc_score(y_true_list, y_scores_list)
+    auc = roc_auc_score(y_true, y_scores)
     
-    print(f"Valid epoch: {epoch}, correct: {correct}, total: {total}, ACC: {acc:.2f}, AUC: {auc:.2f}\n")
+    print(f"Valid epoch: {cur_epoch}, correct: {correct}, total: {total}, ACC: {acc:.2f}, AUC: {auc:.2f}\n")
     
     return acc, auc
 
@@ -290,91 +295,270 @@ def train(cfg):
         csv_path=train_csv_path,
         transform=True
         )
-    print(f"Train dataset length: {len(train_dataset)}")
-    
     val_dataset = DukeDatasetClassification(
         csv_path=val_csv_path,
         transform=True
         )
-    print(f"Val dataset length: {len(val_dataset)}")
-    
     test_dataset = DukeDatasetClassification(
         csv_path=test_csv_path,
         transform=True
         )
+    
+    print(f"Train dataset length: {len(train_dataset)}")
+    print(f"Val dataset length: {len(val_dataset)}")
     print(f"test_dataset dataset length: {len(test_dataset)}")
     
     # 데이터 로더 선언
     train_loader = DataLoader(
-        dataset=train_dataset, 
-        batch_size=cfg.params.batch_size, 
-        shuffle=True, 
+        dataset=train_dataset,
+        batch_size=cfg.params.batch_size,
+        shuffle=True,
         num_workers=4
-        )
+    )
     val_loader = DataLoader(
-        dataset=val_dataset, 
-        batch_size=cfg.params.batch_size, 
-        shuffle=False, 
+        dataset=val_dataset,
+        batch_size=cfg.params.batch_size,
+        shuffle=False,
         num_workers=4
-        )
+    )
     test_loader = DataLoader(
-        dataset=test_dataset, 
-        batch_size=cfg.params.batch_size, 
-        shuffle=False, 
+        dataset=test_dataset,
+        batch_size=cfg.params.batch_size,
+        shuffle=False,
         num_workers=4
-        )
+    )
 
     # 모델 선언
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = load_model(device, mode="resnet18")
+    # model = load_model(device, mode="resnet18")
+    model = resnet18()    
+    model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    num_ftrs = model.fc.in_features # 마지막 계층의 입력 특징의 수를 가져옴
+    model.fc = nn.Linear(num_ftrs, 1)  # 마지막 계층을 새로운 클래스 수에 맞게 교체 (여기서는 10개 클래스)
+    model = model.to(device)
     
     # 옵티마이저 선언
     optimizer = optim.Adam(model.parameters(), lr=cfg.params.lr)
     
     # loss 함수 선언
     loss_func = nn.BCELoss()
+    
+    # --------------- 멀쩡한 예시 -----------------#
+    # Train
+    for epoch in range(50):
+        epoch_loss = 0
+        train_total = 0
+        train_correct = 0
+        for idx, data in enumerate(train_loader):
+            inputs, targets, _ = data
+            inputs = inputs.to(device=device, dtype=torch.float32)
+            targets= targets.to(device=device, dtype=torch.float32).unsqueeze(-1)
 
-    # 에포크 수만큼 훈련 루프를 실행합니다.
-    for epoch in range(0, cfg.params.epochs):
-        # Train fold, Test fold 계산
-        model, cur_loss = train_one_epoch(
-            cfg=cfg,
-            model=model,
-            optimizer=optimizer,
-            loss_func=loss_func,
-            train_loader=train_loader,
-            batch_size=cfg.params.batch_size,
-            epoch=epoch,
-            device=device
-            )
+            logit = model(inputs)
+            prob = torch.sigmoid(logit)
+            loss = loss_func(prob, targets)
 
-        cur_acc, val_auc = valid_one_epoch(model, val_loader, epoch, device)
+            # 값이 0인 텐서를 만든 후, 임계값을 기준으로 값을 1로 설정
+            threshold = prob > 0.5
+            predicted = torch.zeros_like(prob)
+            predicted[threshold] = 1.0
+
+            train_total += predicted.size(0)
+            train_correct += (predicted == targets).sum().item()
+
+            # backpropagation을 위해 gradient를 0으로 설정합니다.
+            optimizer.zero_grad()
+            loss.backward()
+            
+            # optimization 수행
+            optimizer.step()
+            epoch_loss += loss.item()
+
+        train_acc = 100.0 * (train_correct / train_total)
+        print(f"Train_acc: {train_acc}, correct: {train_correct}, total: {train_total}")
         
-        # epoch별 acc, auc, training loss를 딕셔너리에 저장
-        loss_and_auc_each_epoch[epoch] = {
-            'accuracy': round(cur_acc, 3),
-            'auc': round(val_auc, 3), 
-            'loss': round(cur_loss, 3)
-            }
+        # validation acc 계산
+        total = 0
+        correct = 0
+        y_scores_list = list()
+        y_true_list = list()
+        for idx, data in enumerate(val_loader):
+            inputs, targets, _ = data
+            inputs = inputs.to(device=device, dtype=torch.float32)
+            targets= targets.to(device=device, dtype=torch.float32).unsqueeze(-1)
 
-        # best 모델 저장
-        if val_auc > best_auc:
-            if os.path.exists(cfg.paths.model_save_path):
-                os.makedirs(cfg.paths.model_save_path, exist_ok=True)                
-            best_auc = val_auc
-            best_model_save_path = f"{cfg.paths.model_save_path}/model-{epoch}.pth"
+            with torch.no_grad():
+                logit = model(inputs)
+            prob = torch.sigmoid(logit)
+            threshold = prob > 0.5
+            predicted = torch.zeros_like(prob)
+            predicted[threshold] = 1.0
+
+            total += predicted.size(0)
+            correct += (predicted == targets).sum().item()
+            
+            log_targets = targets.squeeze(-1)
+            log_predicted = predicted.squeeze(-1)
+            # print("Targets: ", log_targets)
+            # print("Predicted: ", log_predicted)
+            
+            y_scores_list.extend(prob.detach().cpu().numpy())
+            y_true_list.extend(targets.cpu().numpy())
+            
+        # AUC 및 Epoch 평균 loss 계산
+        # 모든 배치에 대한 예측 확률과 실제 레이블을 하나의 배열로 합치기
+        y_scores = np.array(y_scores_list)
+        y_true = np.array(y_true_list)
+        
+        auc_scroe = roc_auc_score(y_true, y_scores)
+        epoch_loss = round(epoch_loss / len(train_loader), 3)
+            
+        acc = 100.0 * (correct / total)
+        print(f"Validation ACC: {acc}, correct: {correct}, total: {total}")
+        print(f"Validation AUC: {auc_scroe}")
+    #  --------------- 멀쩡한 예시 -----------------#
+
+    #  --------------- 멀쩡한 예시2 -----------------#
+    # # 에포크 수만큼 훈련 루프를 실행합니다.
+    # for epoch in range(cfg.params.epochs):
+    #     print(f'Starting training: epoch {epoch}')
+    #     model.train()
+        
+    #     y_scores_list = list()
+    #     y_true_list = list()
+    #     epoch_loss = 0.0
+    #     train_total = 0   
+    #     train_correct = 0
+
+    #     # 훈련 데이터에 대해 DataLoader를 반복합니다.
+    #     for idx, data in enumerate(train_loader):
+    #         inputs, targets, _ = data
+    #         inputs = inputs.to(device=device, dtype=torch.float32)
+    #         targets= targets.to(device=device, dtype=torch.float32).unsqueeze(-1)
+
+    #         logit = model(inputs)
+    #         prob = torch.sigmoid(logit)
+    #         loss = loss_func(prob, targets)
+
+    #         # 값이 0인 텐서를 만든 후, 임계값을 기준으로 값을 1로 설정
+    #         threshold = prob > 0.5
+    #         predicted = torch.zeros_like(prob)
+    #         predicted[threshold] = 1.0
+
+    #         train_total += predicted.size(0)
+    #         train_correct += (predicted == targets).sum().item()
+
+    #         # backpropagation을 위해 gradient를 0으로 설정합니다.
+    #         optimizer.zero_grad()
+    #         loss.backward()
+            
+    #         # optimization 수행
+    #         optimizer.step()
+    #         epoch_loss += loss.item()
+
+    #         if idx % cfg.params.batch_size == cfg.params.batch_size-1:
+    #             batch_loss = loss.item()
+    #             print(f"Loss after mini-batch {idx + 1}: {batch_loss:.3f}")
+
+    #     acc = 100.0 * (train_correct / train_total)
+    #     print("Train ACC: ", acc)
+
+
+    #     val_total = 0
+    #     val_correct = 0
+    #     for idx, data in enumerate(val_loader):
+    #         # 입력을 가져오기
+    #         inputs, targets, _ = data
+    #         inputs = inputs.to(device=device, dtype=torch.float32)
+    #         targets = targets.to(device=device, dtype=torch.float32).unsqueeze(-1)
+
+    #         # forward pass 수행
+    #         logit = model(inputs)        
+    #         prob = torch.sigmoid(logit)
+            
+    #         loss = loss_func(prob, targets)
+
+    #         # 값이 0인 텐서를 만든 후, 임계값을 기준으로 값을 1로 설정
+    #         pivot = prob > cfg.params.threshold
+    #         predicted = torch.zeros_like(prob)
+    #         predicted[pivot] = 1.0
+
+    #         # backpropagation을 위해 gradient를 0으로 설정합니다.
+    #         optimizer.zero_grad()
+    #         loss.backward()
+            
+    #         # optimization 수행
+    #         optimizer.step()
+
+    #         # loss값을 출력
+    #         epoch_loss += loss.item()
+            
+    #         val_total += predicted.size(0)
+    #         val_correct += (predicted == targets).sum().item()
+            
+    #         # ROC 계산을 위해 값 저장)
+    #         y_scores_list.extend(prob.detach().cpu().numpy())
+    #         y_true_list.extend(targets.cpu().numpy())
+            
+    #         if idx % cfg.params.batch_size == cfg.params.batch_size-1:
+    #             batch_loss = loss.item()
+    #             print(f"Loss after mini-batch {idx + 1}: {batch_loss:.3f}")
+                
+    #     print("Val ACC: ", 100.0 * (val_correct / val_total))
+    #     print("Val Total: ", val_total)
+    #     print("Val Correct: ", val_correct)
+
+    #     # 모든 배치에 대한 예측 확률과 실제 레이블을 하나의 배열로 합치기
+    #     y_scores = np.array(y_scores_list)
+    #     y_true = np.array(y_true_list)
+
+    #     # AUC 및 Epoch 평균 loss 계산
+    #     auc_scroe = roc_auc_score(y_true, y_scores)
+    #     epoch_loss = round(epoch_loss / len(train_loader), 3)
+        
+    #     print(f"Val AUC: {auc_scroe:.3f}")
+    #     print(f"epoch mean loss: {epoch_loss} \n")
+        #  --------------- 멀쩡한 예시2 -----------------#
+    
+    # for epoch in range(cfg.params.epochs):
+    #     model, cur_loss = train_one_epoch(
+    #         cfg=cfg,
+    #         model=model,
+    #         optimizer=optimizer,
+    #         loss_func=loss_func,
+    #         train_loader=train_loader,
+    #         batch_size=cfg.params.batch_size,
+    #         epoch=epoch,
+    #         device=device
+    #         )
+
+    #     cur_acc, val_auc = valid_one_epoch(model, val_loader, epoch, device)
+        
+    #     # epoch별 acc, auc, training loss를 딕셔너리에 저장
+    #     loss_and_auc_each_epoch[epoch] = {
+    #         'accuracy': round(cur_acc, 3),
+    #         'auc': round(val_auc, 3), 
+    #         'loss': round(cur_loss, 3)
+    #         }
+
+    #     # best 모델 저장
+    #     if val_auc > best_auc:
+    #         if os.path.exists(cfg.paths.model_save_path):
+    #             os.makedirs(cfg.paths.model_save_path, exist_ok=True)                
+    #         best_auc = val_auc
+    #         best_model_save_path = f"{cfg.paths.model_save_path}/model-{epoch}.pth"
                     
-            utility.save_model(model, model.state_dict(), best_model_save_path)
-            print('--------------------------------')
-            print(f"Best Val AUC: {best_auc:.2f}, Current Val AUC: {val_auc:.2f}")
-            print(f"!!best model saved!! epoch: {epoch}, Val ACC:{cur_acc:.2f}, Val AUC:{val_auc:.2f}")
-            print('--------------------------------\n')
+    #         utility.save_model(model, model.state_dict(), best_model_save_path)
+    #         print('--------------------------------')
+    #         print(f"Best Val AUC: {best_auc:.2f}, Current Val AUC: {val_auc:.2f}")
+    #         print(f"!!best model saved!! epoch: {epoch}, Val ACC:{cur_acc:.2f}, Val AUC:{val_auc:.2f}")
+    #         print('--------------------------------\n')
 
-        test_acc, test_auc = test_one_epoch(
-            cfg=cfg,
-            model=model,
-            test_loader=test_loader,
-            epoch=epoch,
-            device=device,
-            is_save_csv=True
-        )
+    #     test_acc, test_auc = test_one_epoch(
+    #         cfg=cfg,
+    #         model=model,
+    #         test_loader=test_loader,
+    #         epoch=epoch,
+    #         device=device,
+    #         is_save_csv=True
+    #     )
