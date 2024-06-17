9fc0e2e8450c6b8683ef5fc80001a03abe5d3afb
js <somadubel@gmail.com>
2024-06-17 17:10:42 +0000

    [Feat: classification] AUC, Sensitivity, Specificity 계산

--- srcs/classification/main.py
+++ srcs/classification/main.py
@@ -1,9 +1,10 @@
 from DiffuAug.srcs.classification.train.train import train
+from DiffuAug.srcs.classification.metrics.roc_curve import *
 from DiffuAug.srcs import utility
 
 def main():
     YAML_PATH = r"/workspace/DiffuAug/exp_settings/configs/classification/resnet18_total_200.yaml"
-    OPTION = "train"
+    OPTION = "test"
     
     utility.set_seed()
     cfg = utility.load_config(YAML_PATH)    
@@ -11,6 +12,13 @@ def main():
     
     if OPTION == "train":
         train(cfg)
+        
+    elif OPTION == "test":
+        pred_result_csv_path = r"/data/results/classification/predicted_csv/resnet18/balanced_200/predicted_4.csv"
+        save_curve_png_path = r"/data/results/classification/plot"
+    
+        draw_roc_curve(pred_result_csv_path, save_curve_png_path)
+        compute_acc_with_slices(pred_result_csv_path)
 
 
 if __name__ == '__main__':
