fda23704b1bb0f87858cd998d56162179871c0b4
js <somadubel@gmail.com>
2024-06-30 19:02:10 +0000

    [Refactor: sagan] FID 계산 코드 반환 값 받도록 수정 및 Precision, Recall 계산 시 원본과 동일한 샘플 개수를 사용하도록 변경

--- srcs/generation/metrics/met_runner.py
+++ srcs/generation/metrics/met_runner.py
@@ -13,19 +13,20 @@ def main():
     
     if OPTION == "FID":
         DUKE_DATA_PATH = r"/data/duke_data/size_64/split_datalabel"
-        FAKE_DATA_PATH = r"/data/results/generation/sampling/cfg/imbalanced/sampling_imgs/ddim/epoch_70/p_uncond_0.2/w_4.0"
+        FAKE_DATA_PATH = r"/data/results/generation/sampling/cfg/imbalanced/sampling_imgs/ddim/epoch_70/p_uncond_0.1/w_4.0"
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
         # FID 계산
-        compute_fid(
+        fid_result = compute_fid(
             device=device,
             original_data_path=DUKE_DATA_PATH,
             generated_data_path=FAKE_DATA_PATH
             )
+        print(f"FID Score: {fid_result:.2f}")
         
     elif OPTION == "PRECISION_RECALL":
-        PATH_REAL = r'/data/duke_data/size_64/split_datalabel'
-        PATH_FAKE = r'/data/results/generation/sampling/cfg/imbalanced/sampling_imgs/ddim/epoch_70/p_uncond_0.2/w_0.0'
+        RANDOM_BALANCED_ORIGIN_DATA_PATH = r"/data/duke_data/size_64/random_balanced_origin_data"
+        PATH_FAKE = r'/data/results/generation/sampling/cfg/imbalanced/sampling_imgs/ddim/epoch_70/p_uncond_0.1/w_4.0'
         batch_size = 50 # 기존 코드의 defulat 값
         k = 3 # 기존 코드의 defulat 값
         num_samples = 2600 # real, fake에서 feature를 추출할 이미지의 개수
@@ -34,7 +35,7 @@ def main():
         ipr = IPR(batch_size, k, num_samples)
         with torch.no_grad():
             # real
-            ipr.compute_manifold_ref(PATH_REAL) # manifold_ref값을 구해 self.manifold_ref에 저장
+            ipr.compute_manifold_ref(RANDOM_BALANCED_ORIGIN_DATA_PATH) # manifold_ref값을 구해 self.manifold_ref에 저장
             if len(fname_precalc) > 0:
                 ipr.save_ref(fname_precalc)
                 print('path_fake (%s) is ignored for precalc' % PATH_FAKE)
@@ -51,7 +52,7 @@ def main():
         DUKE_DATA_PATH = r"/data/duke_data/size_64/split_datalabel"
         RANDOM_BALANCED_ORIGIN_DATA_PATH = r"/data/duke_data/size_64/random_balanced_origin_data"
         
-        SAMPLING_ROOT_PATH = r"/data/results/generation/sampling/cfg/imbalanced/sampling_imgs/ddim/epoch_70/p_uncond_0.1"
+        SAMPLING_ROOT_PATH = r"/data/results/generation/sampling/cfg/imbalanced/sampling_imgs/ddim/epoch_70/p_uncond_0.2"
         
         utility.set_normal_logger(LOG_PATH)
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
