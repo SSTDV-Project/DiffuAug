c9b0ab759606d9d4b00c38a9e55e6c67ce93c056
js <somadubel@gmail.com>
2024-06-30 02:53:03 +0000

    [Move: cfg] 이미지 생성 cofng 파일 폴더 이동 및 정리

--- srcs/generation/cfg/img_generation/run_img_gen.py
+++ srcs/generation/cfg/img_generation/run_img_gen.py
@@ -3,8 +3,8 @@ from DiffuAug.srcs import utility
 
 
 def main():
-    GEN_SETTING_YAML_PATH = r"/workspace/DiffuAug/srcs/generation/cfg/img_generation/configs/imbalanced/p_uncond_0.1/w_0.0-4.0_neg-pos.yaml"
-    OPTION = 'ddim_class'
+    GEN_SETTING_YAML_PATH = r"/workspace/DiffuAug/srcs/generation/cfg/img_generation/configs/ddpm/ddpm_cond_neg-pos.yaml"
+    OPTION = 'ddpm_class'
     
     utility.set_seed()
     
@@ -13,15 +13,15 @@ def main():
 
     if OPTION == 'ddim_class':
         # Class로 분간된 DDIM 이미지 생성
-        # generate_cond_ddim_img(cfg, cfg.paths.model_path)
+        generate_cond_ddim_img(cfg, cfg.paths.model_path)
 
         # 지정된 w 값까지 이미지 생성
-        for i in range(40):
-            cfg.cfg_params.w = round(cfg.cfg_params.w + 0.1, 1)
+        # for i in range(40):
+        #     cfg.cfg_params.w = round(cfg.cfg_params.w + 0.1, 1)
 
-            for class_num in range(2):
-                cfg.cfg_params.sampling_class_num = class_num                
-                generate_cond_ddim_img(cfg, cfg.paths.model_path)   
+        #     for class_num in range(2):
+        #         cfg.cfg_params.sampling_class_num = class_num                
+        #         generate_cond_ddim_img(cfg, cfg.paths.model_path)   
             
     elif OPTION == 'ddpm_class': 
         # class로 분간된 DDPM 이미지 생성
